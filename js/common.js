23bc917a931eef38104bf9e2555342abb7b8180f
Yoshitaka-Nagai <129018871+Yoshitaka-Nagai@users.noreply.github.com>
2023-06-26 23:58:23 +0900

    ヘッダー作成途中

--- js/common.js
+++ js/common.js
@@ -1,4 +1,13 @@
 //共通用JS
 document.addEventListener('DOMContentLoaded', function () {
+  //ハンバーガーアイコン
+  const hamburger = document.getElementById('hamburger-menu');
 
+  //スライドメニュー
+  const slideMenu = document.getElementById('slide-menu');
+
+  //ハンバーガーアイコンクリック時処理
+  hamburger.addEventListener('click', function () {
+    slideMenu.classList.toggle('opened');
+  });
 }, false);
