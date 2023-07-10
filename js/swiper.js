a331df0957e35c838e1c34e7916e46fec01b11cb
Yoshitaka-Nagai <129018871+Yoshitaka-Nagai@users.noreply.github.com>
2023-07-10 13:24:02 +0900

    picture-sliderセクション作成

--- js/swiper.js
+++ js/swiper.js
@@ -11,3 +11,21 @@ const mvSwiper = new Swiper('.mv .swiper', {
     waitForTransition: false,
   },
 });
+
+//picture-sliderのスライダー設定
+const pictureSwiper = new Swiper('.picture-slider .swiper', {
+  spaceBetween: 0,
+  slidesPerView: 1,
+  breakpoints: {
+    1280: {
+      slidesPerView: 2,
+    },
+  },
+  centeredSlides: true,
+  loop: true,
+  speed: 9000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+});
