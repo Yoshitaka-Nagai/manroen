3b433103445f7cd6c39da23242c92b5f57d27014
Yoshitaka-Nagai <129018871+Yoshitaka-Nagai@users.noreply.github.com>
2023-07-10 19:33:03 +0900

    スライドメニューhover処理、トップページデザイン修正

--- js/common.js
+++ js/common.js
@@ -6,9 +6,30 @@ document.addEventListener('DOMContentLoaded', function () {
   //スライドメニュー
   const slideMenu = document.getElementById('slide-menu');
 
+  //スライドメニューリンク
+  const slideMenuLink = document.querySelectorAll('.slide-menu__link');
+
   //ハンバーガーアイコンクリック時処理
   hamburger.addEventListener('click', function () {
     this.classList.toggle('opened');
     slideMenu.classList.toggle('opened');
+  });
+
+  slideMenuLink.forEach(link => {
+    link.addEventListener('click', function(){
+      hamburger.classList.remove('opened');
+      slideMenu.classList.remove('opened');
+    });
+    link.addEventListener('mouseover', function(){
+      slideMenuLink.forEach(link => {
+        link.classList.add('no-focus');
+      });
+      link.classList.remove('no-focus');
+    });
+    link.addEventListener('mouseleave', function(){
+      slideMenuLink.forEach(link => {
+        link.classList.remove('no-focus');
+      });
+    });
   });
 }, false);
